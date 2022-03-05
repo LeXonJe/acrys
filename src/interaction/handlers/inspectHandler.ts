f1c9c939691c90cefed988503f23f74f34726c72
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2022-03-05 18:28:18 +0100

    Added basic functionality to gather most used champions

--- src/interaction/handlers/inspectHandler.ts
+++ src/interaction/handlers/inspectHandler.ts
@@ -1,6 +1,6 @@
 import { CacheType, Client, CommandInteraction, MessageEmbed } from "discord.js";
 import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
-import { MatchDto, MatchV5DTOs } from "twisted/dist/models-dto";
+import { ChampionMasteryDTO, MatchDto, MatchV5DTOs, SummonerLeagueDto, SummonerV4DTO } from "twisted/dist/models-dto";
 import { LolUtils } from "../../lol/utils";
 import { Handler } from "../handler";
 
@@ -14,52 +14,94 @@ export const inspectHandler: Handler = {
       description: "Der Name des Spielers",
       required: true,
     },
+    {
+      type: ApplicationCommandOptionTypes.INTEGER.valueOf(),
+      name: "depth",
+      description: "Die Anzahl an Matches, die abgerufen werden",
+      required: false,
+    },
   ],
   execute: async function (client: Client<boolean>, lolUtils: LolUtils, interaction: CommandInteraction<CacheType>): Promise<void> {
     interaction.deferReply();
 
     const ddragonVer = await lolUtils.getLatestDataDragonVersion();
 
-    const profile = await lolUtils.getCompleteSummonerProfile(interaction.options.getString("name", true));
+    let profile: {
+      summoner: SummonerV4DTO;
+      league: SummonerLeagueDto[];
+    };
 
-    const matches = await lolUtils.retrieveMatchInformation(profile.summoner.puuid, 50);
+    try {
+      profile = await lolUtils.getCompleteSummonerProfile(interaction.options.getString("name", true));
+    } catch (e) {
+      const errorEmbed = new MessageEmbed()
+        .setColor("#ED254E")
+        .setTitle(`:boom: An error occured!`)
+        .setDescription(`Couldn't find the player you were searching for.`);
+
+      interaction.editReply({ embeds: [errorEmbed] });
+      return;
+    }
+
+    const matchCount = interaction.options.getInteger("depth") || 5;
+
+    const mastery = await lolUtils.getMasteryPoints(profile.summoner.id);
+    const matches = await lolUtils.getMostUsedChampions(profile.summoner.puuid, matchCount);
+
+    let masteryString = "";
+
+    for (let i = 0; i < mastery.length && i < 10; i++) {
+      masteryString += `${i + 1}. ${mastery[i].champion.name} - Lvl ${mastery[i].championLevel} (${mastery[i].championPoints} Points)\n`;
+    }
+
+    const fields = [
+      { name: ":rosette: Level", value: `➤ ${profile.summoner.summonerLevel.toString()}`, inline: true },
+      {
+        name: ":trophy: Rank",
+        value: `➤ ${profile.league[0].tier} ${profile.league[0].rank} (${profile.league[0].leaguePoints} LP)`,
+        inline: true,
+      },
+      { name: ":tada: Ranked Stats", value: `➤ ${profile.league[0].wins} Wins / ${profile.league[0].losses} Losses`, inline: true },
+      { name: ":medal: Mastery", value: masteryString, inline: true },
+      { name: `:golf: Last Matches (0 out of ${matchCount})`, value: "Loading...", inline: false },
+    ];
 
     const profileEmbed = new MessageEmbed()
-      .setColor("#0099ff")
-      .setTitle(profile.summoner.name)
+      .setColor("#F9DC5C")
+      .setTitle(`${profile.summoner.name}`)
       .setThumbnail(`https://ddragon.leagueoflegends.com/cdn/${ddragonVer}/img/profileicon/${profile.summoner.profileIconId}.png`)
-      .setDescription(`${profile.league[0].wins} Wins / ${profile.league[0].losses} Losses`)
-      .setFields(
-        { name: "Level", value: profile.summoner.summonerLevel.toString(), inline: true },
-        { name: "Rank", value: `${profile.league[0].tier} (${profile.league[0].leaguePoints} LP)`, inline: true },
-        { name: "Matches", value: `Loading Matchinfo...`, inline: false }
-      );
+      .setDescription(``)
+      .setFooter({ text: "Analyzed by Acrys", iconURL: "https://cdn.lexlab.duckdns.org/moron/logo.png" })
+      .setTimestamp()
+      .addFields(fields);
 
-    interaction.editReply({ embeds: [profileEmbed] });
+    interaction.editReply({ embeds: [profileEmbed], content: ":satellite: Retrieving information...\n" });
 
-    let matchString = "";
-    const matchList: MatchV5DTOs.MatchDto[] = [];
+    let matchesCompared = 0;
 
     matches.subscribe({
-      next(match) {
-        matchList.push(match);
-        matchString += `${match.info.participants[0].riotIdName} (${match.info.gameId})\n`;
+      next(mostUsedChamps) {
+        matchesCompared++;
+
+        let matchString = "";
+        let idsSorted = Object.keys(mostUsedChamps).sort((a, b) => mostUsedChamps[b].used - mostUsedChamps[a].used);
+
+        for (let i = 0; i < idsSorted.length && i < 10; i++) {
+          const champion = mostUsedChamps[idsSorted[i]];
+          matchString += `${i + 1}. ${champion.champName} (${Math.round((champion.used / matchesCompared) * 100)}%)\n`;
+        }
+
+        fields[4] = { name: `:golf: Last Matches (${matchesCompared} out of ${matchCount})`, value: matchString, inline: true };
 
         interaction.editReply({
-          embeds: [
-            profileEmbed.setFields(
-              { name: "Level", value: profile.summoner.summonerLevel.toString(), inline: true },
-              { name: "Rank", value: `${profile.league[0].tier} (${profile.league[0].leaguePoints} LP)`, inline: true },
-              { name: "Matches", value: matchString, inline: false }
-            ),
-          ],
+          embeds: [profileEmbed.setFields(fields)],
         });
       },
       error(error) {
         interaction.editReply({ content: error });
       },
       complete() {
-        interaction.editReply("Done!");
+        interaction.editReply(":white_check_mark: Done!");
       },
     });
   },
--- src/lol/utils.ts
+++ src/lol/utils.ts
@@ -7,6 +7,16 @@ interface ChampionMasteryExtended extends ChampionMasteryDTO {
   champion: ChampionsDataDragonDetailsSolo;
 }
 
+interface UsedChampion {
+  champId: number;
+  champName: string;
+  used: number;
+}
+
+interface MostUsedChampions {
+  [id: string]: UsedChampion;
+}
+
 export class LolUtils {
   private lolApi: LolApi;
   private region: Regions;
@@ -22,6 +32,45 @@ export class LolUtils {
     return (await this.lolApi.DataDragon.getVersions())[0];
   }
 
+  async predictPicks(puuid: string, mastery: ChampionMasteryExtended[], count: number = 5) {}
+
+  getMostUsedChampions(puuid: string, depth: number) {
+    const mostUsedChamps: MostUsedChampions = {};
+
+    return new Observable((subscriber: Subscriber<MostUsedChampions>) => {
+      (async () => {
+        (await this.retrieveMatchInformation(puuid, depth)).subscribe({
+          next(match) {
+            for (let i = 0; i < match.info.participants.length; i++) {
+              const participant = match.info.participants[i];
+
+              if (participant.puuid === puuid) {
+                if (!mostUsedChamps[participant.championId]) {
+                  mostUsedChamps[participant.championId] = {
+                    champId: participant.championId,
+                    champName: participant.championName,
+                    used: 1,
+                  };
+                } else {
+                  mostUsedChamps[participant.championId].used++;
+                }
+
+                subscriber.next(mostUsedChamps);
+                break;
+              }
+            }
+          },
+          error(e) {
+            subscriber.error(e);
+          },
+          complete() {
+            subscriber.complete();
+          },
+        });
+      })();
+    });
+  }
+
   async retrieveMatchInformation(puuid: string, count: number = 20) {
     const matches = (
       await this.lolApi.MatchV5.list(puuid, this.regionGroup, {
@@ -31,39 +80,49 @@ export class LolUtils {
 
     return new Observable((subscriber: Subscriber<MatchV5DTOs.MatchDto>) => {
       (async () => {
-        for (let i = 0; i < matches.length; i++) {
-          const matchInfo = (await this.lolApi.MatchV5.get(matches[i], this.regionGroup)).response;
+        try {
+          for (let i = 0; i < matches.length; i++) {
+            const matchInfo = (await this.lolApi.MatchV5.get(matches[i], this.regionGroup)).response;
 
-          subscriber.next(matchInfo);
+            subscriber.next(matchInfo);
 
-          if (i == matches.length - 1) {
-            subscriber.complete();
+            if (i == matches.length - 1) {
+              subscriber.complete();
+            }
           }
+        } catch (e) {
+          subscriber.error(e);
         }
       })();
     });
   }
 
-  async getCompleteSummonerProfile(summonername: string) {
-    const summoner = (await this.lolApi.Summoner.getByName(summonername, this.region)).response;
-    const league = (await this.lolApi.League.bySummoner(summoner.id, this.region)).response;
-    const mastery = (await this.lolApi.Champion.masteryBySummoner(summoner.id, this.region)).response;
+  async getMasteryPoints(summonerId: string) {
+    const mastery = (await this.lolApi.Champion.masteryBySummoner(summonerId, this.region)).response;
 
     const extendedMastery: ChampionMasteryExtended[] = [];
 
     for (let i = 0; i < mastery.length; i++) {
-      const dataDragonChampion = await this.lolApi.DataDragon.getChampion(mastery[i].championId);
+      try {
+        const dataDragonChampion = await this.lolApi.DataDragon.getChampion(mastery[i].championId);
 
-      extendedMastery.push({
-        ...mastery[i],
-        champion: dataDragonChampion,
-      });
+        extendedMastery.push({
+          ...mastery[i],
+          champion: dataDragonChampion,
+        });
+      } catch (e) {}
     }
+
+    return extendedMastery;
+  }
+
+  async getCompleteSummonerProfile(summonername: string) {
+    const summoner = (await this.lolApi.Summoner.getByName(summonername, this.region)).response;
+    const league = (await this.lolApi.League.bySummoner(summoner.id, this.region)).response;
 
     return {
       summoner,
       league,
-      mastery,
     };
   }
 }
